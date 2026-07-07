4bd4a16f83b6e7e7142ebc0bfdf6015b28ed730b
Dylan Perera <96567613+dylanperera@users.noreply.github.com>
2026-07-07 17:27:29 -0400

    Add: design-hashmap - submission-12

---
+++ Data Structures & Algorithms/design-hashmap/submission-12.py
@@ -0,0 +1,83 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+
+class MyHashMap:
+
+    def __init__(self):
+        self.hash_map = [None] * 13337
+
+    def put(self, key: int, value: int) -> None:
+        bucket = key % 13337
+
+        if self.hash_map[bucket] == None:
+            self.hash_map[bucket] = Node(key, value)
+        else:
+            currPtr = self.hash_map[bucket]
+
+            if currPtr.key == key:
+                currPtr.value = value
+                return
+
+            while currPtr.next != None:
+                
+                if currPtr.key == key:
+                    currPtr.value = value
+                    return
+
+                currPtr = currPtr.next
+
+                currPtr.next = Node(key, value)
+        
+
+    def get(self, key: int) -> int:
+        bucket = key % 13337
+        if self.hash_map[bucket] is None:
+            return -1
+
+        currPtr = self.hash_map[bucket]
+
+        while currPtr != None:
+            if currPtr.key == key:
+                return currPtr.value
+
+            currPtr = currPtr.next
+
+        return -1
+
+
+    def remove(self, key: int) -> None:
+        
+        bucket = key % 13337
+
+        head = self.hash_map[bucket]
+
+        if head is None:
+            return
+
+        currPtr = head
+
+        print(key, currPtr.key)
+
+        if currPtr.key == key:
+            print(key)
+            self.hash_map[bucket] = currPtr.next
+            return 
+        
+        while currPtr.next is not None:
+            if currPtr.next.key == key:
+                currPtr.next = currPtr.next.next
+            
+            currPtr = currPtr.next
+
+        return
+
+
+# Your MyHashMap object will be instantiated and called as such:
+obj = MyHashMap()
+obj.put(1,2)
+param_2 = obj.get(1)
+print(param_2)
+# obj.remove(key)
